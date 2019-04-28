406ebe4878c206504218b84af831b312d20585cd
Alan Triacca <atriacca@yahoo.com>
2019-04-28 09:59:53 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+document.form.addEventListener("submit", function(e){
+    e.preventDefault();
+    var num1 = document.add.input1.value;
+    var num2 = document.add.input2.value;
+    document.add.
+    
+    var result = input1 + input2;
+
+    console.log(result)
+})
+/*
+You are going to create a simple calculator that can Add, Subtract, and Multiply.
+
+Requirements:
+
+You will have one section (<div>) for each math operation (so 3 sections) total
+Each section has two inputs to take the first and second numbers
+Each section will have a button to perform the operation
+You will inject the result into the HTML (not an alert)
+Your website will have 3 colors
+Your website will have proper padding/spacing to lay things out nicely
+
+    console.log(e.target.value)
+
+*/
